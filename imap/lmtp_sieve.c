fd8e3efabf9e065cf4a2a54d89428b266b02b7e9
Ken Murchison <murch@fastmail.com>
2019-01-15 21:23:55 -0500

    user.c: user_sieve_path() shouldn't append a trailing '/'

--- imap/lmtp_sieve.c
+++ imap/lmtp_sieve.c
@@ -1638,7 +1638,7 @@ static int sieve_find_script(const char *user, const char *domain,
             if (!script) { /* default script */
                 char *bc_fname;
 
-                strlcat(fname, "defaultbc", size);
+                strlcat(fname, "/defaultbc", size);
 
                 bc_fname = sieve_getdefaultbcfname(fname);
                 if (bc_fname) {
@@ -1650,7 +1650,7 @@ static int sieve_find_script(const char *user, const char *domain,
             }
         }
 
-        snprintf(fname+len, size-len, "%s.bc", script);
+        snprintf(fname+len, size-len, "/%s.bc", script);
     }
 
     /* don't do this for ~username ones */
